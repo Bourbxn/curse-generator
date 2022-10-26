d445ac44161ea5092b2760e8575f22bc11504f4e
Bourbxn <jakkapat97@gmail.com>
2022-10-26 20:57:44 +0700

    re struct front

--- frontend/my-app/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="w-screen h-screen flex justify-center items-center text-5xl">
-      Hello
-    </div>
-  );
-}
-
-export default App;
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screnn flex justify-center items-center text-5xl">
+      Hello React
+    </div>
+  );
+}
+
+export default App;
